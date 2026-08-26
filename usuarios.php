585df3fee6fe981c0ddc5cef84e427628912f3f6
Laboratorio <laboratorio@escola.local>
2026-08-26 10:48:39 -0300

    instalação banco de dados

---
+++ usuarios.php
@@ -0,0 +1,20 @@
+<?php include "cabecalho.php"; ?>
+
+<div class="row">
+    <div class="col-12">
+        <div class="card">
+            <div class="card-header">
+                Pesquisar Usuários
+            </div>
+            <div class="card-body">
+                <div class="row">
+                    <div class="col-2">
+                        <a href="novoUsuario.php" class="btn btn-sucess">Novo usuário</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php include "rodape.php" ?>
